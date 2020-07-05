a5aba371831ddc15d98f20ab44db42c5a0ffa470
Narek <nareksar077@gmail.com>
2020-07-05 15:41:21 +0400

    new change

--- src/main/java/com/scrapper/lazadaBack/api/ItemController.java
+++ src/main/java/com/scrapper/lazadaBack/api/ItemController.java
@@ -48,53 +48,54 @@ public class ItemController {
   }
 
   //http://localhost:8080/cancel?id=123-1&value=text1
+  //http://localhost:8080/cancel?id=309878338714916&value=Ended
   @CrossOrigin(origins = "*")
   @RequestMapping("/cancel")
   @ResponseBody
-  public String cancelTheOrder(@RequestParam String order_id, @RequestParam String value) {
+  public String cancelTheOrder(@RequestParam String  id, @RequestParam String value) {
     System.out.println("ItemController: cancelTheOrder()");
-    System.out.println("Resp: " + order_id + " " + value);
-    return itemService.cancelTheOrder(order_id, value);
+    System.out.println("Resp: " +  id + " " + value);
+    return itemService.cancelTheOrder( id, value);
   }
 
   //http://localhost:8080/invoice?id=123-2&value=text2
   @CrossOrigin(origins = "*")
   @RequestMapping("/invoice")
   @ResponseBody
-  public String setInvoiceNumber(@RequestParam String order_id, @RequestParam String value) {
+  public String setInvoiceNumber(@RequestParam String  id, @RequestParam String value) {
     System.out.println("ItemController: setInvoiceNumber()");
-    System.out.println("Resp: " + order_id + " " + value);
-    return itemService.setInvoiceNumber(order_id, value);
+    System.out.println("Resp: " +  id + " " + value);
+    return itemService.setInvoiceNumber( id, value);
   }
 
   //http://localhost:8080/packed?id=123-3&value=text3
   @CrossOrigin(origins = "*")
   @RequestMapping("/packed")
   @ResponseBody
-  public String markPacked(@RequestParam String order_id, @RequestParam String value) {
+  public String markPacked(@RequestParam String id, @RequestParam String value) {
     System.out.println("ItemController: markPacked()");
-    System.out.println("Resp: " + order_id + " " + value);
-    return itemService.markPacked(order_id, value);
+    System.out.println("Resp: " + id + " " + value);
+    return itemService.markPacked(id, value);
   }
 
   //http://localhost:8080/delivered?id=123-4&value=text4
   @CrossOrigin(origins = "*")
   @RequestMapping("/delivered")
   @ResponseBody
-  public String markDelivered(@RequestParam String order_id, @RequestParam String value) {
+  public String markDelivered(@RequestParam String id, @RequestParam String value) {
     System.out.println("ItemController: markDelivered()");
-    System.out.println("Resp: " + order_id + " " + value);
-    return itemService.markDelivered(order_id, value);
+    System.out.println("Resp: " + id + " " + value);
+    return itemService.markDelivered(id, value);
   }
 
   //http://localhost:8080/ship?id=123-5&value1=text1&value2=text2
   @CrossOrigin(origins = "*")
   @RequestMapping("/ship")
   @ResponseBody
-  public String markReadyToShip(@RequestParam String order_id, @RequestParam String value1, @RequestParam String value2) {
+  public String markReadyToShip(@RequestParam String id, @RequestParam String value1, @RequestParam String value2) {
     System.out.println("ItemController: markReadyToShip()");
-    System.out.println("Resp: " + order_id + " " + value1 + " " + value2);
-    return itemService.markReadyToShip(order_id, value1, value2);
+    System.out.println("Resp: " + id + " " + value1 + " " + value2);
+    return itemService.markReadyToShip(id, value1, value2);
   }
 
   //http://localhost:8080/
